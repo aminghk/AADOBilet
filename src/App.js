76e52c4a050e9f6b2e79ee1cc970f777db3f4764
anilates97 <anilates.97@gmail.com>
2023-10-22 17:39:18 +0300

    added responsive design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import AadoButton from "./components/AadoButton";
-import HeaderTitle from "./components/HeaderForm/HeaderTitle";
-import HeaderView from "./components/HeaderForm/HeaderView";
+import HeaderView from "./components/Header/HeaderView";
 
 function App() {
   return <HeaderView />;
